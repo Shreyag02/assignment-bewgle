c6edfd5cb92b43e06fa851be9c0592d6febad6ab
Shreya-Gupta02 <42897236+Shreya-Gupta02@users.noreply.github.com>
2020-10-24 01:48:30 +0530

    Updates

---
+++ precache-manifest.d20130462c97c0f41581d4c69d3e0be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d1cd5059188c020febbcdedcd10a1a",
+    "url": "/assignment-bewgle/index.html"
+  },
+  {
+    "revision": "cf5985b15a15080ec962",
+    "url": "/assignment-bewgle/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "763193649de74a156392",
+    "url": "/assignment-bewgle/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "cf5985b15a15080ec962",
+    "url": "/assignment-bewgle/static/js/2.ac6821e7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/assignment-bewgle/static/js/2.ac6821e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763193649de74a156392",
+    "url": "/assignment-bewgle/static/js/main.84571013.chunk.js"
+  },
+  {
+    "revision": "ec686966fe128711bb67",
+    "url": "/assignment-bewgle/static/js/runtime-main.08bd20f4.js"
+  }
+]);
